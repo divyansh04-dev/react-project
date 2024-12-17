361c03570f44d4422e9489c37d7565ec09262d63
Divyansh Jaiswal <work.divyansh7303@gmail.com>
2024-12-17 18:01:23 +0530

    learn tailwind and props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card';
 
 function App() {
+  const data = {
+    name: ['divyansh', 'aryan'],
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className='bg-white text-black rounded-xl p-4'>
-          Divyansh Jaiswal
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <Card name="divyansh" />
+      <Card name="aryan" />
     </div>
   );
 }
